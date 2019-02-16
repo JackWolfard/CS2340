b619b0b92c9e0dbbb91923b487fa265ce2580b0d
Jack Wolfard <jackwolfard@gatech.edu>
2019-02-15 22:55:29 -0500

    setup android project and added basic outline to complete M5

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/model/Game.java
@@ -0,0 +1,17 @@
+package edu.gatech.cs2340.spacetrader.model;
+
+import edu.gatech.cs2340.spacetrader.entity.GameDifficulty;
+
+public class Game {
+
+    private Player player;
+    private GameDifficulty difficulty;
+
+    /**
+     * Default empty constructor which initializes everything to dummy
+     * values before the player is created or loaded
+     */
+    public Game() {
+
+    }
+}
---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/model/Model.java
@@ -0,0 +1,26 @@
+package edu.gatech.cs2340.spacetrader.model;
+
+/**
+ *  Similar to M3, this is a Singleton Pattern which allows for us to have only one instance
+ *  of the game at a time and can access allow our VM to access the backend.
+ */
+public class Model {
+
+    private Game game;
+
+    private static Model instance = new Model();
+
+    /**
+     * getter for instance
+     *
+     * @return the singleton instance
+     */
+    public static Model getInstance() {
+        return instance;
+    }
+
+    private Model() {
+        game = new Game();
+    }
+
+}
