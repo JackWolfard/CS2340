42ef2642c479b4da2201da713d0e85d9b0f68e94
Bradley <bradley.allnutt.2@gmail.com>
2019-02-16 20:57:20 -0500

    Implimented the game difficulty enum

--- app/src/main/java/edu/gatech/cs2340/spacetrader/entity/GameDifficulty.java
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/entity/GameDifficulty.java
@@ -1,4 +1,36 @@
 package edu.gatech.cs2340.spacetrader.entity;
 
+import android.icu.lang.UCharacter;
+
 public enum GameDifficulty {
+    B ("Beginner"),
+    E ("Easy"),
+    N ("Normal"),
+    H ("Hard"),
+    I ("Impossible");
+
+    /** the full string representation of the difficulty */
+    private final String difficulty;
+
+    /**
+     * Constructor for the enumeration
+     *
+     * @param difficulty   full name of the course
+     */
+    GameDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    /**
+     *
+     * @return   the full course name
+     */
+    public String getDifficulty() { return difficulty; }
+
+    /**
+     *
+     * @return the display string representation of the course
+     */
+    public String toString() { return difficulty; }
+
 }
