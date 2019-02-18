aa94f8bce7f6834db1fe942e4108821cd7d7ce2b
josephmeredith <43077689+josephmeredith@users.noreply.github.com>
2019-02-18 14:43:14 -0500

    Mostly finished UI

--- app/src/main/java/edu/gatech/cs2340/spacetrader/view/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/view/ConfigurationActivity.java
@@ -2,6 +2,8 @@ package edu.gatech.cs2340.spacetrader.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.EditText;
 
 import edu.gatech.cs2340.spacetrader.viewmodel.ConfigurationViewModel;
 import edu.gatech.cs2340.spacetrader.R;
@@ -9,6 +11,10 @@ import edu.gatech.cs2340.spacetrader.R;
 public class ConfigurationActivity extends AppCompatActivity {
 
     private ConfigurationViewModel configVM;
+
+    private EditText nameEntry;
+    private EditText pilotPt;
+    private EditText engineerPt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
