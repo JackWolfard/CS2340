5b4efa2f3d03b142eccd530b86e1ea4efa8e6106
Jack Wolfard <jackwolfard@gatech.edu>
2019-02-18 22:50:54 -0500

    Finished adapters for view elements and added logic for on pressed for plaeyr

--- app/src/main/java/edu/gatech/cs2340/spacetrader/view/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/view/ConfigurationActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -18,9 +19,10 @@ public class ConfigurationActivity extends AppCompatActivity {
 
     private ConfigurationViewModel configVM;
 
+    private EditText nameField;
     private EditText pilotField;
-    private EditText traderField;
     private EditText engineerField;
+    private EditText traderField;
     private EditText fighterField;
     private Spinner difficultySpinner;
 
@@ -29,17 +31,30 @@ public class ConfigurationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config);
 
+        nameField = findViewById(R.id.enter_name);
         pilotField = findViewById(R.id.pilot_point_entry);
-        traderField = findViewById(R.id.engineer_point_entry);
         engineerField = findViewById(R.id.engineer_point_entry);
+        traderField = findViewById(R.id.trader_point_entry);
+        fighterField = findViewById(R.id.fighter_point_entry);
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
+
+        ArrayAdapter<GameDifficulty> difficultyAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, GameDifficulty.values());
+        difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(difficultyAdapter);
 
         viewModel = ViewModelProviders.of(this).get(ConfigurationViewModel.class);
     }
 
     public void onAddPressed(View view) {
         Log.d("Edit", "Create Player Pressed");
-        GameDifficulty difficulty =
-        initGame();
+        GameDifficulty difficulty = (GameDifficulty) difficultySpinner.getSelectedItem();
+        viewModel.initGame(difficulty, nameField.getText().toString(), convertToInt(pilotField),
+                convertToInt(engineerField), convertToInt(traderField), convertToInt(fighterField));
+    }
+
+    private int convertToInt(android.widget.EditText widget) {
+        return Integer.parseInt(widget.getText().toString());
     }
 
 
