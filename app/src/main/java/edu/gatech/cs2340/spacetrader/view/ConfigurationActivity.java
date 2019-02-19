2d8f39fa507db751f1e09fb7064ebdfb80a0c083
josephmeredith <43077689+josephmeredith@users.noreply.github.com>
2019-02-18 21:21:06 -0500

    lil stuff for bradley

--- app/src/main/java/edu/gatech/cs2340/spacetrader/view/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/view/ConfigurationActivity.java
@@ -2,6 +2,7 @@ package edu.gatech.cs2340.spacetrader.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.EditText;
 
@@ -15,10 +16,20 @@ public class ConfigurationActivity extends AppCompatActivity {
     private EditText nameEntry;
     private EditText pilotPt;
     private EditText engineerPt;
+    private EditText traderPt;
+    private EditText fighterPt;
+    private Spinner difficultySelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config);
+
+        nameEntry = findViewById(R.id.enter_name);
+        pilotPt = findViewById(R.id.pilot_point_entry);
+        engineerPt = findViewById(R.id.engineer_point_entry);
+        traderPt = findViewById(R.id.trader_point_entry);
+        fighterPt = findViewById(R.id.fighter_point_entry);
+        difficultySelect = findViewById(R.id.difficulty_spinner);
     }
 }
