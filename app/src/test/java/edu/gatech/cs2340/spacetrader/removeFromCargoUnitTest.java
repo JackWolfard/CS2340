628598b96edcc02abf475236f26c233abffbdcfe
Nboerner <nboerner3@gatech.edu>
2019-04-08 21:34:15 -0400

    Added removeFromCargoUnitTest JUnit

---
+++ app/src/test/java/edu/gatech/cs2340/spacetrader/removeFromCargoUnitTest.java
@@ -0,0 +1,38 @@
+package edu.gatech.cs2340.spacetrader;
+
+import org.junit.Test;
+
+import edu.gatech.cs2340.spacetrader.entity.Goods;
+import edu.gatech.cs2340.spacetrader.entity.ShipType;
+import edu.gatech.cs2340.spacetrader.model.Ship;
+
+import static org.junit.Assert.assertEquals;
+
+public class removeFromCargoUnitTest {
+    Ship testShip = new Ship(ShipType.GN);
+
+    @Test
+    public void initialize() {
+        assertEquals(0,testShip.getCurrentsize());
+    }
+
+    @Test
+    public void removeSuccessful() {
+        testShip.addToCargo(Goods.FOOD);
+        testShip.removeFromCargo(Goods.FOOD);
+
+        assertEquals(0,testShip.getCurrentsize());
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void cargoFullRemove() {
+        for (int i = 0; i < 16; i++) {
+            testShip.addToCargo(Goods.FIREARMS);
+        }
+        assertEquals(15, testShip.getCurrentsize());
+        for (int j = 0; j < 16; j++) {
+            testShip.removeFromCargo(Goods.FIREARMS);
+        }
+        assertEquals(0, testShip.getCurrentsize());
+    }
+}
